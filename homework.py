66d15f51ecb987572c90e362047d2a780a9d930c
Павел Киселев <neznika2@yandex.ru>
2022-02-10 16:13:11 +0300

    Sprint 7 Homework repair

--- homework.py
+++ homework.py
@@ -1,11 +1,12 @@
+import logging
 import os
 import time
-from dotenv import load_dotenv
-import telegram
-import requests
-import logging
 from http import HTTPStatus
 from logging import StreamHandler
+
+import requests
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -28,7 +29,7 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 
 
-HOMEWORK_STATUSES = {
+VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -40,57 +41,66 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.info(f'Бот отправил сообщение {message}')
-    except Exception:
-        logger.error('Ошибка при отправке сообщения ботом!')
+    except Exception as error:
+        logger.exception('Ошибка при отправке сообщения ботом!', exc_info=error)
 
 
 def get_api_answer(current_timestamp):
     """Делаем запрос к API и возвращаем JSON."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if response.status_code != HTTPStatus.OK:
-        msg = (f'Эндпоинт {ENDPOINT} недоступен. '
-               f'Код ответа API: {response.status_code}')
-        logger.error(msg)
-        raise Exception
-    else:
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+        if response.status_code != HTTPStatus.OK:
+            logger.error(f'Эндпоинт {ENDPOINT} недоступен.')
+            raise Exception
         return response.json()
+    except Exception as error:
+        msg = (f'Эндпоинт {ENDPOINT} недоступен. '
+                f'Код ответа API: {response.status_code}')
+        logger.exception(msg, exc_info=error)
+        raise Exception
+
 
 
 def check_response(response):
     """Возвращаем список работ."""
+    try:
+        response['homeworks']
+    except Exception as error:
+        logger.exception('Некорректный ключ словаря', exc_info=error)
     if not isinstance(response['homeworks'], list):
         logger.error('Некорректные данные от API')
         raise Exception
-    else:
-        return response['homeworks']
+    return response['homeworks']
 
 
 def parse_status(homework):
     """Получаем статус домашней работы."""
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-    if homework_status not in HOMEWORK_STATUSES.keys():
+    
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_name == 'None' or homework_status == 'None':
+        logger.error('Ошибка ключей словаря')
+    if homework_status not in VERDICTS.keys():
         logger.error('Недокументированный статус домашней работы')
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверяем наличие всех токенов."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    else:
-        logger.critical('Отсутствует обязательная переменная окружения')
-        return False
+    list_var = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
+    return all(list_var)
 
 
 def main():
     """Основная логика работы бота."""
-    check_tokens()
+    if not check_tokens():
+        logger.critical('Нет необходимых токенов!')
+        raise Exception
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time())-60*60*24*30
     while True:
         try:
             response = get_api_answer(current_timestamp)
@@ -106,8 +116,6 @@ def main():
             message = f'Сбой в работе программы: {error}'
             logger.error(message)
             time.sleep(RETRY_TIME)
-        else:
-            return False
 
 
 if __name__ == '__main__':
